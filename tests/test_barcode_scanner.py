7306e2483dacdc1f2b67f7f673e69517aefeb925
Davide Marziano <64016958+davimarz@users.noreply.github.com>
2026-09-14 18:08:16 +0200

    Aggiunge test bollino Aspirina C

--- tests/test_barcode_scanner.py
+++ tests/test_barcode_scanner.py
@@ -9,3 +9,12 @@ def test_candidates_keep_scanned_ean_and_extract_likely_aic():
 
 def test_candidates_normalize_short_aic():
     assert medicine_lookup_candidates("12345678") == ["12345678", "012345678"]
+
+
+def test_candidates_extract_aspirina_c_aic_from_bollino():
+    assert medicine_lookup_candidates("A004763114")[0] == "004763114"
+
+
+def test_candidates_extract_aic_from_multifield_datamatrix():
+    candidates = medicine_lookup_candidates("A004763114\x1d072050014")
+    assert candidates[0] == "004763114"
